470377dea2fd5158394b3db05d6b8560814e655e
Tamim-Amin <tamimamin13579@gmail.com>
2024-12-03 17:05:55 +0600

    Update FloyedWarshell.cpp

--- FloyedWarshell.cpp
+++ FloyedWarshell.cpp
@@ -1,75 +1,89 @@
- #include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define INF INT_MAX
-
 int path[100][100];
-
-void printPath(int s, int d) {
-    if (s == d) {
-        cout << d << " ";
-    } else {
-        printPath(s, path[s][d]);
-        cout << d << " ";
+void printPath(int s,int d)
+{
+    if(s==d)
+    {
+        cout<<d<<" ";
     }
+    else{
+        printPath(s,path[s][d]);
+        cout<<d<<" ";
+    }
+
 }
 
-int main() {
-    int n, e, u, v, w, i, s;
-
-    cout << "Enter number of vertices: ";
-    cin >> n;
-    cout << "Enter number of edges: ";
-    cin >> e;
+int main()
+{
+    int n,e;
+    cout<<"Enter vertex and edge: ";
+    cin>>n>>e;
 
     int dist[n][n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                dist[i][j] = 0;
-                path[i][j] = i;
-            } else {
-                dist[i][j] = INF;
-                path[i][j] = -1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j)
+            {
+                dist[i][j]=0;
+                path[i][j]=i;
+            }
+            else
+            {
+                dist[i][j]=INF;
+                path[i][j]=-1;
             }
         }
     }
 
-    cout << "Enter edges with source, destination, and weight (S D W):\n";
-    for (int i = 0; i < e; i++) {
-        cin >> u >> v >> w;
-        dist[u][v] = w;
-        path[u][v] = u;
+    int u,v,w;
+    for(int i=0;i<e;i++)
+    {
+        cin>>u>>v>>w;
+        dist[u][v]=w;
+        path[u][v]=u;
     }
 
-    // Floyd-Warshall algorithm
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][j] > dist[i][k] + dist[k][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                    path[i][j] = path[k][j];
+    for(int k=0;k<n;k++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(dist[i][k]!=INF && dist[k][j]!=INF && dist[i][j]>dist[k][j]+dist[i][k])
+                {
+                    dist[i][j]=dist[k][j]+dist[i][k];
+                    path[i][j]=path[k][j];
                 }
             }
         }
     }
 
-    cout << "\nDistance matrix:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF)
-                cout << "INF ";
-            else
-                cout << dist[i][j] << " ";
+    cout<<"Distance matrix: ";
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(dist[i][j]==INF)
+            {
+                cout<<"INF";
+            }
+            else{
+                cout<<dist[i][j]<<" ";
+            }
         }
-        cout << endl;
+        cout<<endl;
     }
 
-    int src, dest;
-    cout << "\nEnter source and destination: ";
-    cin >> src >> dest;
-    cout << "Shortest Path: ";
-    printPath(src, dest);
-    cout << "\nCost: " << dist[src][dest] << endl;
+    int s,d;
+    cin>>s>>d;
+    cout<<"Shortest path: ";
+    printPath(s,d);
+    cout<<"Cost: "<<dist[s][d];
 
-    return 0;
+
+
 }
